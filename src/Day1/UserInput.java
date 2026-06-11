5916bc0c5eb26067b75b9eea1a82b2317c3727ed
Rupam Mondal <mondalrupam756@gmil.com>
2026-06-11 22:42:32 +0530

    commited

---
+++ src/Day1/UserInput.java
@@ -0,0 +1,15 @@
+package Day1;
+
+import java.util.Scanner;
+ 
+public class UserInput {
+public static void main(String[] args) {
+	Scanner myObj = new Scanner(System.in);
+	System.out.print
+	("Enter username : ");
+	
+	String username = myObj.nextLine();
+    System.out.println("User name is " + username);
+}
+}
+
